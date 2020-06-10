1381e1f89f87a0773b8aa08d13ead6aca0d21fb2
남우석 <hinyong22@gmail.com>
2020-06-11 00:20:14 +0900

    받아쓰끼 등록 완료

--- vue-dictation/src/components/Teacher.vue
+++ vue-dictation/src/components/Teacher.vue
@@ -1,22 +1,22 @@
 <template>
   <v-container align-center>
  <v-form class="mt-12 pt-4 pr-4 pl-4">
-   <v-file-input label="File input"></v-file-input>
-   <div v-for="item in 10" :key="item"> 
-     <span>{{item + ". 받아쓰기 정답등록"}}</span>
+  <v-file-input @change="processFile($event)" label="upload"></v-file-input>
+   <div v-for="item in answers" :key="item.question_no"> 
+     <span>{{item.question_no + ". 받아쓰기 정답등록"}}</span>
     <v-text-field 
               label="정답"
               outlined
-              v-model="title"
+              v-model="item.question"
               dense>
     </v-text-field>
   </div>
    <!-- <v-textarea
     
       label="질문글"
-    ></v-textarea> -->
+    ></v-textarea>-- -->
      <v-flex class="text-center">
-        <v-btn width="270" color="primary" x-large>등록</v-btn>
+        <v-btn width="270" color="primary" x-large @click="test()">등록</v-btn>
     </v-flex>
  </v-form>
 </v-container>
@@ -24,7 +24,66 @@
 <script>
 export default {
   data: () => ({
-    title: ''
-  })
+    answers: [
+      {question_no: 1, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null },
+      {question_no: 2, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 3, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 4, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 5, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 6, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 7, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 8, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 9, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 10, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      ],
+      fileTest: ''
+  }),
+  methods: {
+    test() {
+      // const formData = new FormData();
+      
+      // formData.append("year",null);
+      // formData.append("term",null);
+      // formData.append("lecture_no",1);
+      // formData.append("course_no",1);
+      // formData.append("question_no",1);
+      // formData.append("file", this.fileTest);
+      // formData.append("file_save_nm",null);
+      // formData.append("input_id",null);
+      // formData.append("updated_id",null);
+      // formData.append("update_date",null);
+
+     
+
+      for(let item of this.answers) {
+        console.log(item);
+        this.$http.post('/api/course',item).then(res => {
+          console.log(res);
+        }).catch(err => {
+          console.error(err);
+        })
+      }
+     
+      // // console.log(formData);
+      // this.$http.post('/api/course',testObj).then(res => {
+      //   console.log(res);
+      // }).catch(err => {
+      //   console.error(err);
+      // })
+      //파일 저장
+      // this.$http.post('/api/course/fileupload',formData, {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data'
+      //   }
+      // }).then(res => {
+      //   console.log(res);
+      // }).catch(err => {
+      //   console.error(err);
+      // })
+    },
+    processFile(file) {
+      this.fileTest = file;
+    }
+  }
 }
 </script>
