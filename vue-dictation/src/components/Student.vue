525ca20aedbcdf75daab8db27e2a2c101394d3ca
남우석 <hinyong22@gmail.com>
2020-06-16 22:23:17 +0900

    받아쓰기 완성

--- vue-dictation/src/components/Student.vue
+++ vue-dictation/src/components/Student.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container align-center>
+  <audio src="../assets/dictation.wav" controls></audio>
  <v-form class="mt-12 pt-4 pr-4 pl-4">
    <div v-for="item in answers" :key="item.question_no"> 
      <span>{{item.question_no + ".  받아쓰기"}}</span>
@@ -7,63 +8,81 @@
               label="정답"
               outlined
               v-model="item.question"
+              :error="item.error"
               dense>
     </v-text-field>
   </div>
-   <!-- <v-textarea
-    
-      label="질문글"
-    ></v-textarea>-- -->
+  
      <v-flex class="text-center">
-        <v-btn width="270" color="primary" x-large @click="test()">제출</v-btn>
+        <v-btn width="270" color="primary" x-large @click="sumbit()">제출</v-btn>
     </v-flex>
  </v-form>
+  <v-dialog v-model="dialog" persistent max-width="290">
+    <v-card>
+          <v-card-title class="headline">당신의 점수는?</v-card-title>
+          <v-card-text>{{this.score}} 점입니다.</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="again()">다시하기</v-btn>
+            <v-btn color="green darken-1" text @click="goLecture()">그만하기</v-btn>
+          </v-card-actions>
+    </v-card>
+  </v-dialog>
 </v-container>
+
 </template>
 <script>
+import router from '../router'
 export default {
   data: () => ({
+    dialog: false,
+    score: 0,
     answers: [
-      {question_no: 1, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null },
-      {question_no: 2, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 3, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 4, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 5, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 6, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 7, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 8, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 9, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
-      {question_no: 10, question: '', year: "2020", term: "1", lecture_no: 1, course_no: 1, file_nm: null, file_save_nm: null, input_id: null, updated_id: null,update_date: null},
+      {question_no: 1, question: '', lecture_no: 1, course_no: 1,hint:"",error:false },
+      {question_no: 2, question: '', lecture_no: 1, course_no: 1,hint:"",error:false},
+      {question_no: 3, question: '',lecture_no: 1, course_no: 1,hint:"",error:false},
+      {question_no: 4, question: '',  lecture_no: 1, course_no: 1, hint:"",error:false},
+      {question_no: 5, question: '',lecture_no: 1, course_no: 1, hint:"",error:false},
+      {question_no: 6, question: '', lecture_no: 1, course_no: 1,hint:"", error:false},
+      {question_no: 7, question: '',lecture_no: 1, course_no: 1,hint:"", error:false},
+      {question_no: 8, question: '', lecture_no: 1, course_no: 1,hint:"", error:false},
+      {question_no: 9, question: '', lecture_no: 1, course_no: 1,hint:"", error:false},
+      {question_no: 10, question: '', lecture_no: 1, course_no: 1,hint:"", error:false},
       ],
-      fileTest: ''
+     
   }),
   methods: {
-    test() {
-      // const formData = new FormData();
-      
-      // formData.append("year",null);
-      // formData.append("term",null);
-      // formData.append("lecture_no",1);
-      // formData.append("course_no",1);
-      // formData.append("question_no",1);
-      // formData.append("file", this.fileTest);
-      // formData.append("file_save_nm",null);
-      // formData.append("input_id",null);
-      // formData.append("updated_id",null);
-      // formData.append("update_date",null);
+    sumbit() {
 
-     
-
-      for(let item of this.answers) {
-        console.log(item);
-        this.$http.post('/api/course',item).then(res => {
-          console.log(res);
-        }).catch(err => {
-          console.error(err);
-        })
-      }
+     this.score = 0;
+    let i =0;
+      this.$http.post('api/enroll/answer', this.answers).then(res => {
+        for(let answer of res.data) {
+          
+          if(answer) {
+            this.score += 10;
+          }else {
+            this.answers[i].error = true;
+          }
+          i++;
+        }
+        this.dialog = true;
+      }).catch( err => {
+        console.log(err);
+      })
+    
      
     
+    },
+    again() {
+      for(let i in this.answers) {
+        this.answers[i].error = false;
+        this.answers[i].question = "";
+      }
+      this.dialog = false
+    },
+    goLecture() {
+      router.push({path: '/readlc'})
     }
   }
 }
